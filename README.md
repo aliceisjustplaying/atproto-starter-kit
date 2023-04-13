6bf0c9d3f968a40f8cd70c9087c357c87dfd03ad
alice <aliceisjustplaying@gmail.com>
2023-04-14 00:25:10 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# bluesky starter kit
+
+Use this as a template repo. Requires Node 18+ with typescript and ts-node installed globally (`npm i -g typescript ts-node`). Put your bluesky username and password to `.env` then run your app with `npm run start` or `ts-node-esm index.ts`.
---
+++ index.ts
@@ -0,0 +1,35 @@
+import bsky from '@atproto/api';
+const { BskyAgent } = bsky;
+import * as dotenv from 'dotenv';
+import process from 'node:process';
+dotenv.config();
+
+const agent = new BskyAgent({
+  service: 'https://bsky.social',
+});
+
+await agent.login({
+  identifier: process.env.BSKY_USERNAME!,
+  password: process.env.BSKY_PASSWORD!,
+});
+
+const bleet =
+  'You can find the code for this bleet >>>here<<<, now with a link card that has a nice title and description!';
+await agent.post({
+  text: bleet,
+  entities: [
+    {
+      index: { start: bleet.indexOf('>>>') + 3, end: bleet.indexOf('<<<') },
+      type: 'link',
+      value: 'https://github.com/aliceisjustplaying/bluesky-starter-kit',
+    },
+  ],
+  embed: {
+    $type: 'app.bsky.embed.external',
+    external: {
+      uri: 'https://github.com/aliceisjustplaying/bluesky-starter-kit',
+      title: "alice's bluesky starter kit",
+      description: "i'm just playing around with the api",
+    },
+  },
+});
